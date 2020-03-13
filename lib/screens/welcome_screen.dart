434453c08ae6ca208d08564b93ce733031db6400
Lucas GILHARD <l.gilhard@gmail.com>
2020-03-13 15:45:22 +0100

    refactor buttons

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/widgets/flash_button.dart';
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
@@ -50,41 +51,23 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.routeName);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
+            FlashButton(
+              child: Text(
+                'Log In',
               ),
+              onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.routeName);
+              },
+              color: Colors.lightBlueAccent,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.routeName);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
+            FlashButton(
+              child: Text(
+                'Register',
               ),
+              onPressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.routeName);
+              },
+              color: Colors.blueAccent,
             ),
           ],
         ),
---
+++ lib/widgets/flash_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FlashButton extends StatelessWidget {
+  FlashButton({@required this.child, @required this.onPressed, this.color});
+
+  final Color color;
+  final Widget child;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
